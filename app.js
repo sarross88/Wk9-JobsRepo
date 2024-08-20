f7a3a2f80b0c5f7a8d97d8bd6fbcdd6762221c05
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-08-20 10:38:05 -0400

    week10 deployment job app

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 require('express-async-errors');
-const express = require('express');
-const app = express();
 
 //Extra Security Packages 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xxs-clean')
+const rateLimiter = require('express-rate-limit')
 
+const express = require('express');
+const app = express();
 
 //Connect DB
 const connectDB = require('./db/connect')
@@ -16,9 +20,18 @@ const jobsRouter = require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
+//Security Middleware for deployment 
+//this one to trust heroku 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `windowMS` (here, per 15 minutes).
+}));
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 
 // routes
 // app.get('/', (req, res) => {
