6d5d02c4763b26730ae90184d569f9067a2026f7
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-08-30 12:57:43 -0400

    backend finalizing

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(xss());
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
-
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
